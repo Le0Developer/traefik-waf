3c3bc5390b59398327d8782526dd9f112ceea7ff
Leo Developer <leo@git.leodev.win>
2026-01-08 19:38:41 +0100

    update jspowobfdata version

--- build/assets/challenge.js
+++ build/assets/challenge.js
@@ -1,145 +1,148 @@
 if (!navigator.cookieEnabled) return alert("Cookies are required to proceed.");
 // MODERN=1 SKEW_EXTRA_OPTIONS="--define:decrypt.wasm.DEFAULT_WASM_URL='{{ASSETS}}/w.wasm' --define:WEBLIB_EXPORT_NAME='solve'" make all
-!(function (e) {
-  function n(e) {
+!(function (n) {
+  function e(n) {
     for (
-      var n = (4294967295 * Math.random()) | 0, t = 0, r = e[0];
+      var e = (4294967295 * Math.random()) | 0, t = 0, r = n[0];
       r > t;
       t = (t + 1) | 0
     )
-      e.slice(1, 33)[t >> 3] ^= ((n >>> t) & 1) << (7 & t);
+      n.slice(1, 33)[t >> 3] ^= ((e >>> t) & 1) << (7 & t);
   }
-  function t(e, f) {
-    var l, d, v, y;
-    "u" > typeof document && "u" > typeof Worker && s
-      ? ((u = (u + 1) | 0),
-        (l = c = (c + 1) | 0),
-        null === a &&
-          ((a = new Worker(
-            "string" == typeof (y = i.src) && "" !== y
-              ? y
-              : "data:text/javascript;," + i.text
+  function t(n, e, r) {
+    crypto.subtle
+      .importKey("raw", n.slice(1, 33), { name: "AES-GCM" }, !1, ["decrypt"])
+      .then(function (e) {
+        return crypto.subtle.decrypt(
+          { name: "AES-GCM", iv: n.slice(33, 45) },
+          e,
+          n.slice(45)
+        );
+      })
+      .then(e)
+      .catch(function () {
+        (function (n) {
+          for (
+            var e = 0, t = 0, r = n[0];
+            r > t && ((e = 1 << (7 & t)), !((n[(1 + (t >> 3)) | 0] ^= e) & e));
+            t = (t + 1) | 0
+          );
+        })(n),
+          t(n, e, (r = (r + 1) | 0));
+      });
+  }
+  function r(n, v) {
+    var d, m, y, p;
+    "u" > typeof document && "u" > typeof Worker && l
+      ? ((s = (s + 1) | 0),
+        (d = f = (f + 1) | 0),
+        null === c &&
+          ((c = new Worker(
+            "string" == typeof (p = u.src) && "" !== p
+              ? p
+              : "data:text/javascript;," + u.text
           )).onerror = function () {
-            s = !1;
+            l = !1;
           }),
-        (d = null),
-        (v = null),
-        (d = function (e) {
-          var n = e.data;
-          n.b ^ l ||
-            (a.removeEventListener("message", d),
-            a.removeEventListener("error", v),
-            (u = (u - 1) | 0) || (null !== a && (a.terminate(), (a = null))),
-            f(n.a));
+        (m = null),
+        (y = null),
+        (m = function (n) {
+          var e = n.data;
+          e.b ^ d ||
+            (c.removeEventListener("message", m),
+            c.removeEventListener("error", y),
+            (s = (s - 1) | 0) || (null !== c && (c.terminate(), (c = null))),
+            v(e.a));
         }),
-        a.addEventListener("message", d),
-        (v = function () {
-          t(e, f);
+        c.addEventListener("message", m),
+        (y = function () {
+          r(n, v);
         }),
-        a.addEventListener("error", v),
-        a.postMessage(new r(l, e, null)))
-      : "u" > typeof WebAssembly
-      ? (null === o &&
-          (o = WebAssembly.instantiateStreaming(fetch("{{ASSETS}}/w.wasm"))),
-        o).then(function (t) {
-          !(function (e, t, r) {
-            n(e);
-            for (
-              var o = e.length,
-                i = r.instance.exports,
-                a = new Uint8Array(i.memory.buffer),
-                u = 0;
-              u < o;
-              u = (u + 1) | 0
-            )
-              a[u] = e[u];
-            i.decrypt(0, o, o + 1);
-            for (
-              var c = e.length - 61, s = new Uint8Array(c), f = 0;
-              c > f;
-              f = (f + 1) | 0
-            )
-              s[f] = a[(1 + o + f) | 0];
-            t(s);
-          })(e, f, t);
-        })
-      : (n(e),
-        (function e(n, t, r) {
-          crypto.subtle
-            .importKey("raw", n.slice(1, 33), { name: "AES-GCM" }, !1, [
-              "decrypt",
-            ])
-            .then(function (e) {
-              return crypto.subtle.decrypt(
-                { name: "AES-GCM", iv: n.slice(33, 45) },
-                e,
-                n.slice(45)
-              );
-            })
-            .then(t)
-            .catch(function () {
-              (function (e) {
-                for (
-                  var n = 0, t = 0, r = e[0];
-                  r > t &&
-                  ((n = 1 << (7 & t)), !((e[(1 + (t >> 3)) | 0] ^= n) & n));
-                  t = (t + 1) | 0
-                );
-              })(n),
-                e(n, t, (r = (r + 1) | 0));
-            });
-        })(e, f, 0));
+        c.addEventListener("error", y),
+        c.postMessage(new o(d, n, null)))
+      : "u" > typeof WebAssembly && a
+      ? (null === i &&
+          (i = WebAssembly.instantiateStreaming(fetch("{{ASSETS}}/w.wasm"))),
+        i).then(
+          function (t) {
+            !(function (n, t, r) {
+              e(n);
+              for (
+                var o = n.length,
+                  a = r.instance.exports,
+                  i = new Uint8Array(a.memory.buffer),
+                  u = 0;
+                u < o;
+                u = (u + 1) | 0
+              )
+                i[u] = n[u];
+              a.decrypt(0, o, o + 1);
+              for (
+                var c = n.length - 61, s = new Uint8Array(c), f = 0;
+                c > f;
+                f = (f + 1) | 0
+              )
+                s[f] = i[(1 + o + f) | 0];
+              t(s);
+            })(n, v, t);
+          },
+          function () {
+            (a = !1), e(n), t(n, v, 0);
+          }
+        )
+      : (e(n), t(n, v, 0));
   }
-  function r(e, n, t) {
-    (this.b = e), (this.c = n), (this.a = t);
+  function o(n, e, t) {
+    (this.b = n), (this.c = e), (this.a = t);
   }
-  e.solve = function (e, n) {
-    t(
-      (function (e) {
-        for (var n = new Uint8Array(128), t = 0; t < 64; t = (t + 1) | 0)
-          n[t < 26 ? t + 65 : t < 52 ? t + 71 : t < 62 ? t - 4 : 4 * t - 205] =
+  n.solve = function (n, e) {
+    r(
+      (function (n) {
+        for (var e = new Uint8Array(128), t = 0; t < 64; t = (t + 1) | 0)
+          e[t < 26 ? t + 65 : t < 52 ? t + 71 : t < 62 ? t - 4 : 4 * t - 205] =
             t;
         for (
-          var r = (function (e) {
-              for (var n = [], t = 0, r = e.length; r > t; t = (t + 1) | 0)
-                n.push(e.charCodeAt(t));
-              return n;
-            })(e),
-            o = e.length,
-            i = new Uint8Array(
+          var r = (function (n) {
+              for (var e = [], t = 0, r = n.length; r > t; t = (t + 1) | 0)
+                e.push(n.charCodeAt(t));
+              return e;
+            })(n),
+            o = n.length,
+            a = new Uint8Array(
               (3 * (o - (61 === r[(o - 1) | 0]) - (61 === r[(o - 2) | 0]))) / 4
             ),
-            a = 0,
+            i = 0,
             u = 0;
-          o > a;
+          o > i;
 
         ) {
-          var c = n[r[a++]],
-            s = n[r[a++]],
-            f = n[r[a++]],
-            l = n[r[a++]];
-          (i[u++] = (c << 2) | (s >> 4)),
-            (i[u++] = ((15 & s) << 4) | (f >> 2)),
-            (i[u++] = ((3 & f) << 6) | l);
+          var c = e[r[i++]],
+            s = e[r[i++]],
+            f = e[r[i++]],
+            l = e[r[i++]];
+          (a[u++] = (c << 2) | (s >> 4)),
+            (a[u++] = ((15 & s) << 4) | (f >> 2)),
+            (a[u++] = ((3 & f) << 6) | l);
         }
-        return i;
-      })(e),
-      function (e) {
-        n(new TextDecoder().decode(new Uint8Array(e)));
+        return a;
+      })(n),
+      function (n) {
+        e(new TextDecoder().decode(new Uint8Array(n)));
       }
     );
   };
-  var o = null,
-    i = "u" > typeof document ? document.currentScript : null,
-    a = null,
-    u = 0,
-    c = 0,
-    s = !0;
+  var a = !0,
+    i = null,
+    u = "u" > typeof document ? document.currentScript : null,
+    c = null,
+    s = 0,
+    f = 0,
+    l = !0;
   "u" < typeof document &&
-    addEventListener("message", function (e) {
-      var n = e.data;
-      t(n.c, function (e) {
-        postMessage(new r(n.b, null, e));
+    addEventListener("message", function (n) {
+      var e = n.data;
+      r(e.c, function (n) {
+        postMessage(new o(e.b, null, n));
       });
     });
 })(this);
